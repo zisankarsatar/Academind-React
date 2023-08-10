31f2052a7a2ba9af27331dca614a42551c3fb72b
Zisan Karsatar <zisankarsatar@gmail.com>
2023-08-10 19:03:01 +0200

    filter and add issue completed

--- homework1/src/App.js
+++ homework1/src/App.js
@@ -1,54 +1,73 @@
 import './App.css';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ExpenseItem from './components/ExpenseItem';
-import NewExpense from './components/NewExpense/NewExpense.js';
+import ExpensesFilter from './components/ExpensesFilter';
+import NewExpense from './components/NewExpense/NewExpense';
+
+const expensesArray =[
+  {
+    id:1,
+    title: 'Car Insurance',
+    amount: 294.23,
+    date: new Date(2021, 5, 16)  
+  },
+  {
+    id:2,
+    title: 'Health Insurance',
+    amount: 180,
+    date: new Date(2019, 4, 23)  
+  },
+  {
+    id:3,
+    title: 'New Kitchen',
+    amount: 1200,
+    date: new Date(2022, 10, 1)  
+  },
+  {
+    id:4,
+    title: 'Education',
+    amount: 30,
+    date: new Date(2019, 8, 25)  
+  },
+  {
+    id:5,
+    title: 'New Department',
+    amount: 294.23,
+    date: new Date(2021, 3, 10)  
+  },
+]
 
 function App() {
-  const expenses =[
-    {
-      id:1,
-      title: 'Car Insurance',
-      amount: 294.23,
-      date: new Date(2021, 5, 16)  
-    },
-    {
-      id:2,
-      title: 'Health Insurance',
-      amount: 180,
-      date: new Date(2000, 4, 23)  
-    },
-    {
-      id:3,
-      title: 'New Kitchen',
-      amount: 1200,
-      date: new Date(2022, 10, 1)  
-    },
-    {
-      id:4,
-      title: 'Education',
-      amount: 30,
-      date: new Date(1999, 8, 25)  
-    },
-    {
-      id:5,
-      title: 'New Department',
-      amount: 294.23,
-      date: new Date(2021, 3, 10)  
-    },
-  ]
+  const [expenses, setExpenses] = useState([]);
+  const [filteredYear, setFilteredYear] = useState('');
 
-  const addExpenseHandler = (enteredExpense) =>{
-    //expenses.push(...expenses, enteredExpense);
-    console.log(enteredExpense);
+  useEffect(() => {
+    let newExpenses  = filteredYear === '' ? expensesArray : expensesArray.filter(expense => expense.date.getFullYear().toString() === filteredYear);
+    setExpenses(newExpenses);
+  },[filteredYear]);
+
+  const addExpenseHandler = enteredExpense =>{
+    let tempArr = expenses;
+    tempArr.push(enteredExpense);
+    setExpenses(tempArr);
+    setFilteredYear(filteredYear);
   }
+
+  const filterChangeHandler = selectedYear => {
+    console.log(selectedYear);
+    selectedYear === '' ? setFilteredYear('') : setFilteredYear(selectedYear)    
+  };
 
   return (
     <div>
       <h2>Let's get started!</h2>
       <NewExpense onAddExpense={addExpenseHandler}/>
-      {expenses.map((expense, index)=>(
-        <ExpenseItem key={index} expense={expense}></ExpenseItem>
-      ))}
+      <div className='expenses'>
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        {expenses.map((expense, index)=>(
+          <ExpenseItem key={index} expense={expense}></ExpenseItem>
+        ))}
+      </div>
     </div>
   );
 }
